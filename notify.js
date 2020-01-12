f86b8f2349289a0883ef69df9b61d4f5ee8d332c
Stella W <peipeiw51@gmail.com>
2020-01-12 09:38:32 -0800

    added css for font and sizing

---
+++ notify.js
@@ -0,0 +1,10 @@
+<head>
+	<script src="/path/to/lib.js"></script>
+</head>
+
+<script>
+    const assignmentscheduler = lib.stellaw1.assignmentscheduler['@dev'];
+
+	let result = await assignmentscheduler.notify();
+
+</script>
